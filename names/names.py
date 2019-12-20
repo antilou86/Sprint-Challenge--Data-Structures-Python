2da7498e3e14f2a390fd2471305dafcda36f2976
antilou86 <antilou86@gmail.com>
2019-12-20 15:09:44 -0500

    all MVP and Stretch complete.

--- names/names.py
+++ names/names.py
@@ -11,21 +11,21 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-#oof, O(n^2)
+
 duplicates = []
+# # oof, O(n^2)
+# # need to change the below to something more efficient
+# # for name_1 in names_1:
+# #     for name_2 in names_2:
+# #         if name_1 == name_2:
+# #             duplicates.append(name_1)
 
-#need to change the below to something more efficient
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
-
-bst = BinarySearchTree(names_1[0])
-for i in range(1, len(names_1)):
-    bst.insert(names_1[i])
-for i in range(len(names_2)):
-    if bst.contains(names_2[i]):
-        duplicates.append(names_2[i])
+# bst = BinarySearchTree(names_1[0])
+# for i in range(1, len(names_1)):
+#     bst.insert(names_1[i])
+# for i in range(len(names_2)):
+#     if bst.contains(names_2[i]):
+#         duplicates.append(names_2[i])
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -37,8 +37,8 @@ print (f"runtime: {end_time - start_time} seconds")
 # structures?
 
 # O(n)
-# names_3 = names_1 + names_2
-# names_3.sort()
-# for i in range(len(names_1)-1):
-#     if names_1[i] == names_1[i+1]:
-#         duplicates.append(names_1[i])
+names_3 = names_1 + names_2
+names_3.sort()
+for i in range(len(names_1)-1):
+    if names_1[i] == names_1[i+1]:
+        duplicates.append(names_1[i])
