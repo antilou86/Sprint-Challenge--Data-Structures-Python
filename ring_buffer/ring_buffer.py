e943aff147d4cb811472c9f272842cac48a333f0
antilou86 <antilou86@gmail.com>
2019-12-20 14:21:54 -0500

    completed stretch for ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -39,10 +39,20 @@ class RingBuffer:
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None] * capacity
 
     def append(self, item):
-        pass
-
+        if self.current == self.capacity:
+            self.current = 0
+        self.storage[self.current]=item
+        self.current+=1
+        
     def get(self):
-        pass
+        tempList = []
+        for i in self.storage:
+            if i is not None:
+                tempList.append(i)
+        return tempList
+                
